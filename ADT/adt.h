243d8b38d6b98c1b08871ec70b7a551b0c06558c
Shashwat Singh <singhshashwat1002@gmail.com>
2021-04-28 19:53:40 +0530

    [ADT.h] make struct definition of mcq questions.
    Also declare function for initialization.

---
+++ ADT/adt.h
@@ -0,0 +1,25 @@
+#ifndef __ADT_H
+#define  __ADT_H
+#include <string.h>
+#include <malloc.h>
+#include <assert.h>
+
+enum supported_question_types = {mcq = 0};
+// more supported types will be added to this enumeration.
+
+struct mcq_question
+{
+    char* question_text;
+    char** option_list;
+    int correct_option; // will have index of the correct option as in the option_list
+    double difficulty;
+};
+
+typedef struct mcq_question McqQuestion;
+
+McqQuestion* initialize_mcq_question(double difficulty, char* question_text, char** option_list, char* correct_option);
+// the above method takes the correct option as a string as well.
+
+void display_mcq_question(McqQuestion* question);
+
+#endif
