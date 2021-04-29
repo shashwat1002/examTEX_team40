b2c17e036a94d4916bf58d611174b239318635fa
Shashwat Singh <singhshashwat1002@gmail.com>
2021-04-29 22:33:50 +0530

    [ADT] implemention: define initialization function for mcq questions.
    Note: for now, each option can't be more than 140 characters long.
    
    Certain declarations have been changed in the adt.h as well.

--- ADT/adt.h
+++ ADT/adt.h
@@ -4,7 +4,7 @@
 #include <malloc.h>
 #include <assert.h>
 
-enum supported_question_types = {mcq = 0};
+enum supported_question_types {mcq = 0};
 // more supported types will be added to this enumeration.
 
 struct mcq_question
@@ -13,11 +13,12 @@ struct mcq_question
     char** option_list;
     int correct_option; // will have index of the correct option as in the option_list
     double difficulty;
+    int total_options;
 };
 
 typedef struct mcq_question McqQuestion;
 
-McqQuestion* initialize_mcq_question(double difficulty, char* question_text, char** option_list, char* correct_option);
+McqQuestion* initialize_mcq_question(double difficulty, char* question_text, int num_options, char option_list[num_options][140], char* correct_option);
 // the above method takes the correct option as a string as well.
 
 void display_mcq_question(McqQuestion* question);
