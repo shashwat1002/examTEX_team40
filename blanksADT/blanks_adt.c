3565c68f75d0c3abab323272bf590f7cb7703742
siddik11803 <82626587+siddik11803@users.noreply.github.com>
2021-05-05 23:39:08 +0530

    Add files via upload

---
+++ blanksADT/blanks_adt.c
@@ -0,0 +1,41 @@
+//
+//  blanks_adt.c
+//  
+//
+//  Created by Siddik Ayyappa on 05/05/21.
+//
+
+#include "blanks_adt.h"
+#include<stdlib.h>
+#include<string.h>
+
+
+questionPtr initializeQuestion(char question_string[], char answer_array[][answerLength], int numberOfAns){
+    questionPtr q = (questionPtr)malloc(sizeof(question));
+    q->numberOfoptions = numberOfAns;
+    strcpy(q->questionString, question_string);
+    q->answerArray = (char**)malloc(sizeof(char*)*numberOfAns);
+//    q->answerArray
+    for(int i = 0; i < numberOfAns; i++){
+        q->answerArray[i] = malloc(sizeof(char)*answerLength);
+        strcpy(q->answerArray[i], answer_array[i]);
+    }
+    return q;
+}
+
+void printQuestion(questionPtr q){
+    printf("\n%s? (%d)\nAnswers:\n", q->questionString, q->numberOfoptions);
+    for(int i = 1; i <= q->numberOfoptions; i++){
+        printf("%d . %s\n", i, q->answerArray[i - 1]);
+    }
+}
+
+
+int main(){
+    char q_string[] = "what is this";
+    char a_array[3][10] = {"That", "tHis", "this"};
+    
+    questionPtr q = initializeQuestion(q_string, a_array, 3);
+    printQuestion(q);
+    return 0;
+}
---
+++ blanksADT/blanks_adt.h
@@ -0,0 +1,23 @@
+//
+//  blanks_adt.h
+//  
+//
+//  Created by Siddik Ayyappa on 05/05/21.
+//
+
+#ifndef blanks_adt_h
+#define blanks_adt_h
+
+#define answerLength 10
+#include <stdio.h>
+
+typedef struct question{
+    char questionString[256];
+    char** answerArray;
+    int numberOfoptions;
+}question;
+typedef question* questionPtr;
+
+questionPtr initializeQuestion(char question_string[], char answer_array[][answerLength], int numberOfAns);
+
+#endif /* blanks_adt_h */
