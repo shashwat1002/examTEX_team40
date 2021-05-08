0937aa4b7c4208cd4a160b230bd7b9bf5ea874de
Shashwat Singh <singhshashwat1002@gmail.com>
2021-05-08 19:57:59 +0530

    introduce two new ADTs in the parser utils
    - one for each sample
    - one which represents the whole file and has a list of samples

--- ParserUtils/utils.h
+++ ParserUtils/utils.h
@@ -7,6 +7,7 @@
 
 #define current_option_limit 140
 
+
 struct question_bank_tree
 {
     McqQuestion** mcq_questions;
@@ -15,6 +16,29 @@ struct question_bank_tree
 };
 
 typedef struct  question_bank_tree ParsedTree;
+typedef enum supported_question_types QuestionType;
+
+struct sample_information
+{
+    QuestionType type; // will have a value as to which question this sample corresponds to
+    double difficulty_lower_bound;
+    double difficulty_upper_bound;
+    int number_of_questions;
+};
+
+typedef struct sample_information SampleInformation;
+
+enum randomization_scheme {global=0, sample_wise=1};
+// global randomization will lead to all questions being picked according to specification first
+// followed by randomization
+// sample wise randomization will randomize questions on a per sample basis.
+
+struct question_paper_specifications
+{
+    SampleInformation** sample_list;
+    enum randomization_scheme scheme;
+    int number_of_papers;
+};
 
 McqQuestion* parse_mcq_question(FILE *question_bank_file);
 
