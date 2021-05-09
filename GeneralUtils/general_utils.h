6f82dfc4f8bb6483fca9da1f9138172c9dbb5444
Avyukta-Manjunatha-Vummintala <82697386+Avyukta-Manjunatha-Vummintala@users.noreply.github.com>
2021-05-09 16:46:45 +0530

    Update general_utils.h
    I think I have made the required modifications.

--- GeneralUtils/general_utils.h
+++ GeneralUtils/general_utils.h
@@ -2,18 +2,7 @@
 #define __GENERAL_UTILS_H
 
 #include <stdlib.h>
-struct mcq_question
-{
-    char *question_text;
-    char **option_list;
-    int correct_option;
-    double difficulty;
-    int total_options;
-};
-
-typedef struct mcq_question McqQuestion;
-
-
+#include "../ADT/adt.h"
 void merge( McqQuestion** arr, int low, int mid, int high);
 void merge_sort(McqQuestion** arr, int low, int high);
 
