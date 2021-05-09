e59cd0cc4f3098e454c5d256810d3b2c9e33e4f9
Avyukta-Manjunatha-Vummintala <82697386+Avyukta-Manjunatha-Vummintala@users.noreply.github.com>
2021-05-09 16:49:28 +0530

    Update generation.h
    I think I have made the required modifications.

--- Generation/generation.h
+++ Generation/generation.h
@@ -7,22 +7,7 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <time.h>
-
-struct mcq_question
-{
-    char *question_text;
-    char **option_list;
-    int correct_option;
-    double difficulty;
-    int total_options;
-};
-
-struct question_bank_tree{
-    McqQuestion** mcq_questions;
-    int num_mcq_questions;
-};
-typedef struct mcq_question McqQuestion;
-typedef struct question_bank_tree ParsedTree;
+#include "../ADT/adt.h"
 
 
 int BinarySearch_LowerBound( ParsedTree* T , double L );
