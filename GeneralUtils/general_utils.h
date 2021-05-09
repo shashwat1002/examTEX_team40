86d40f164d711c683b2b4ba5ea9f213b655f6b37
Avyukta-Manjunatha-Vummintala <82697386+Avyukta-Manjunatha-Vummintala@users.noreply.github.com>
2021-05-09 13:47:46 +0530

    Update general_utils.h

--- GeneralUtils/general_utils.h
+++ GeneralUtils/general_utils.h
@@ -1,4 +1,24 @@
 #ifndef __GENERAL_UTILS_H
 #define __GENERAL_UTILS_H
 
+#include <stdlib.h>
+struct mcq_question
+{
+    char *question_text;
+    char **option_list;
+    int correct_option;
+    double difficulty;
+    int total_options;
+};
+
+typedef struct mcq_question McqQuestion;
+
+
+void merge( McqQuestion** arr, int low, int mid, int high);
+void merge_sort(McqQuestion** arr, int low, int high);
+
+
+
+
+
 #endif
